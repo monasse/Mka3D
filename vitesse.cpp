a00921bf486e1e1f6b74580b13330a9789dd8fd3
Frederic Marazzato <frederic.marazzato@enpc.fr>
2017-05-10 11:40:12 +0200

    Tracation à vitesse constante.

--- vitesse.cpp
+++ vitesse.cpp
@@ -22,8 +22,8 @@
 //Initial velocity of the solid particles
 Vector_3 velocity(const Point_3 &p)
 {
-  return Vector_3(0.01*(p.x()-1.),0,0);
-  //return Vector_3(0,0,0);
+  //return Vector_3(0.01*(p.x()-1.),0,0);
+  return Vector_3(0.01,0,0);
 }
 
 //Initial angular velocity of the solid particles
