ef7f24a999cf6a2a8eb2346b474eb1363e937d3a
Frederic Marazzato <frederic.marazzato@enpc.fr>
2018-09-18 10:27:07 +0200

    La conservation de l'énergie ne va pas du tout quand on rajoute les forces de pénalisation de Hansbo... Doit y avoir un bug. A trouver.

--- src/solide.cpp
+++ src/solide.cpp
@@ -1850,7 +1850,7 @@ void Solide::Forces_internes_bis(const double& dt, const double& theta, const do
     }
 
     //Forces pénalisation
-    /*if(F->fissure < 1) { //On pénalise quand face pas fissurée
+    if(F->fissure < 1) { //On pénalise quand face pas fissurée
       for(int i=0 ; i<solide[voisin1].faces.size() ; i++){
 	if(i != F->id) {
 	  Vector_3 nIJ = faces[i].normale;
@@ -1890,7 +1890,7 @@ void Solide::Forces_internes_bis(const double& dt, const double& theta, const do
 	  faces[i].F = faces[i].F + 2.*mu * eta * faces[i].S / (F->h * solide[voisin2].V) * ((solide[voisin2].discrete_gradient) * faces[i].inertie) * nIJ;
 	}
       }
-    }*/
+    }
 
     
   }
@@ -2088,7 +2088,7 @@ const double Solide::Energie_potentielle_MEMM(const double &t, const double &T){
   }
 
   //Energie de la pénalisation
-  /*for(std::vector<Face>::iterator F=faces.begin();F!=faces.end();F++){
+  for(std::vector<Face>::iterator F=faces.begin();F!=faces.end();F++){
     if(F->fissure < 1) { //On pénalise quand face pas fissurée
       int voisin2 = F->voisins[1];
       if(voisin2 != -1)
@@ -2100,7 +2100,7 @@ const double Solide::Energie_potentielle_MEMM(const double &t, const double &T){
       Ep += mu * eta / F->h * F->S * contraction_double( (solide[F->voisins[0]].discrete_gradient) * F-> inertie, (solide[F->voisins[0]].discrete_gradient).T() );
       Ep += mu * eta / F->h * F->S * contraction_double( (solide[F->voisins[1]].discrete_gradient) * F-> inertie, (solide[F->voisins[1]].discrete_gradient).T() );
     }
-    }*/
+  }
 
   return Ep;
 }
