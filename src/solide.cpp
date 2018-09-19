08b973bcab673262c65d24eae6b7c80a332c620e
Frederic Marazzato <frederic.marazzato@enpc.fr>
2018-09-19 10:52:05 +0200

    Correction d'une erreur dans l'expression de l'énergie et d'une dans l'expression des forces. Conservation qdm toujours pas vérifiée.

--- src/solide.cpp
+++ src/solide.cpp
@@ -1850,14 +1850,14 @@ void Solide::Forces_internes_bis(const double& dt, const double& theta, const do
     }
 
     //Forces pénalisation
-    /*if(F->fissure < 1) { //On pénalise quand face pas fissurée
+    if(F->fissure < 1) { //On pénalise quand face pas fissurée
       for(int i=0 ; i<solide[voisin1].faces.size() ; i++){
 	if(i != F->id) {
 	  Vector_3 nIJ = faces[i].normale;
 	  if(nIJ * Vector_3(solide[voisin1].x0, faces[i].centre) < 0.)
 	    nIJ = -nIJ;
 	  if(voisin2 >= 0)
-	    faces[i].F = faces[i].F + 2.*mu * eta * faces[i].S / (F->h * solide[voisin1].V) * ((solide[voisin1].discrete_gradient - solide[voisin2].discrete_gradient) * faces[i].inertie) * nIJ;
+	    faces[i].F = faces[i].F + 2.*mu * eta * faces[i].S / (F->h * solide[voisin1].V) * ((solide[voisin1].discrete_gradient - solide[voisin2].discrete_gradient) * faces[i].inertie) * nIJ; // -
 	  //else //face au bord
 	    //faces[i].F = faces[i].F + 2.*mu * eta * faces[i].S / (F->h * solide[voisin1].V) * ((solide[voisin1].discrete_gradient) * faces[i].inertie) * nIJ;
 	}
@@ -1868,11 +1868,11 @@ void Solide::Forces_internes_bis(const double& dt, const double& theta, const do
 	    Vector_3 nIJ = faces[i].normale;
 	    if(nIJ * Vector_3(solide[voisin2].x0, faces[i].centre) < 0.)
 	      nIJ = -nIJ;
-	    faces[i].F = faces[i].F + 2.*mu * eta * faces[i].S / (F->h * solide[voisin2].V) * ((solide[voisin1].discrete_gradient - solide[voisin2].discrete_gradient) * faces[i].inertie) * nIJ;
+	    faces[i].F = faces[i].F - 2.*mu * eta * faces[i].S / (F->h * solide[voisin2].V) * ((solide[voisin1].discrete_gradient - solide[voisin2].discrete_gradient) * faces[i].inertie) * nIJ; // +
 	  }
 	}
       }
-    }*/
+    }
     /*else if(F->fissure == 1) {//On pénalise comme si 2 faces étaient sur le bord !
       for(int i=0 ; i<solide[voisin1].faces.size() ; i++){
 	if(i != F->id) {
@@ -2090,9 +2090,9 @@ const double Solide::Energie_potentielle_MEMM(const double &t, const double &T){
   //Energie de la pénalisation
   for(std::vector<Face>::iterator F=faces.begin();F!=faces.end();F++){
     if(F->fissure < 1) { //On pénalise quand face pas fissurée
-      /*int voisin2 = F->voisins[1];
+      int voisin2 = F->voisins[1];
       if(voisin2 != -1)
-      Ep += mu * eta / F->h * F->S * contraction_double( (solide[F->voisins[0]].discrete_gradient - solide[voisin2].discrete_gradient) * F-> inertie, (solide[F->voisins[0]].discrete_gradient - solide[voisin2].discrete_gradient).T() ); */
+	Ep += mu * eta / F->h * contraction_double( (solide[F->voisins[0]].discrete_gradient - solide[voisin2].discrete_gradient) * F-> inertie, (solide[F->voisins[0]].discrete_gradient - solide[voisin2].discrete_gradient).T() ); // * F->S
       /*else if(voisin2 == -1)
 	Ep += mu * eta / F->h * F->S * contraction_double( (solide[F->voisins[0]].discrete_gradient) * F-> inertie, (solide[F->voisins[0]].discrete_gradient).T() ); */
     }
